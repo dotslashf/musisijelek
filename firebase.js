4e5cbe707a0c3e3efd7ab45cafc0203aaabd9a72
dotslashf <fadhlurahman.i@gmail.com>
2021-06-18 19:19:58 +0800

    Add firebase

--- firebase.js
+++ firebase.js
@@ -1,20 +1,30 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json');
+const { getArtist } = require('./spotifyWrapper');
 
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
-
 const db = admin.firestore();
 
-const saveArtist = (name) => {
-  const doc = db.collection('listAlphabet')
-}
+const saveArtist = async (char, name) => {
+  const doc = db.collection('listAlphabet').doc(char);
+  await doc.set({
+    name: admin.firestore.FieldValue.arrayUnion(name),
+  });
+};
 
-const docRef = db.collection('listAlphabet').doc('a');
+const checkArtist = async (char, artist) => {
+  const alphabetRef = db.collection('listAlphabet').doc(char);
+  const doc = await alphabetRef.get();
+  const { name } = doc.data();
+  if (!name.includes(char)) {
+    await saveArtist(char, artist);
+  } else {
+
+  }
+};
 
 (async () => {
-  await docRef.set({
-    first: 'Ada',
-    last: 'Lovelace',
-    born: 1815,
-  });
+  const obj = await getArtist();
+  // console.log(obj);
+  // await checkArtist(obj.char, obj.artist);
 })();
