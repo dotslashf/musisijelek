4d43e9050b64bda2976bca1f70074a970a7d1466
dotslashf <fadhlurahman.i@gmail.com>
2021-06-19 11:05:45 +0800

    Add loading bar

---
+++ utils/helpers.js
@@ -0,0 +1,25 @@
+const cliProgress = require('cli-progress');
+
+const sleep = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+const loadingBar = async nTime => {
+  const bar = new cliProgress.SingleBar(
+    {
+      format: 'Next tweet ' + colors.cyan('{bar}') + ' {value}/{total} Minutes',
+      barCompleteChar: '\u2588',
+      barIncompleteChar: '\u2591',
+    },
+    cliProgress.Presets.rect
+  );
+
+  bar.start(nTime, 0);
+  for (let i = 0; i < nTime; i++) {
+    bar.update(i);
+    await sleep(1000);
+  }
+  bar.stop();
+};
+
+module.exports = { loadingBar };
