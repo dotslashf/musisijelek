51a697a78433bc5a1bb86df6fbc796ab06efa71b
dotslashf <fadhlurahman.i@gmail.com>
2021-07-06 18:02:11 +0800

    Replace string

--- wrapper/firebase.js
+++ wrapper/firebase.js
@@ -4,7 +4,7 @@ const admin = require('firebase-admin');
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
 });
